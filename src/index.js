104372588fa6d04ad98c682ea47775d7575abba2
unknown <rkrajneesh91@gmail.com>
2022-03-08 22:08:05 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Rajneesh:LMPiDV0VO2cXL7Qg@cluster0.wdu0f.mongodb.net/backend?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const userController= require("../controllers/userController")
+const userController= require("../controllers/userController");
+const { tokenCheck } = require('../middleware/auth');
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/users", userController.createUser  )
 
-router.post("/login", userController.loginUser)
-
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-
-router.put("/users/:userId", userController.updateUser)
+router.post("/createUser", userController.createUser  )
+router.post("/loginUser", userController.loginUser)
+router.get("/users/:userId", tokenCheck, userController.getUserData)
+router.put("/users/:userId", tokenCheck, userController.updateUser)
+router.delete("/users/:userId", tokenCheck, userController.deleteData)
 
 module.exports = router;
